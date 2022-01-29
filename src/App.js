0c1e629d62b8aedc039c1240e8065a10e37b4d29
hshoja <hamid.shoja@gmail.com>
2022-01-29 21:45:55 +0300

    feat: add funtionlity to read from local blockchain

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import { useState } from "react";
+import { ethers } from "ethers";
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
+  const [greeting, setGreeting] = useState("");
+
+  //request access to the user's Metamask account
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
+
+  // call the smart contract, read the current greeting value
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+      try {
+        const data = await contract.greet();
+        alert(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: "5rem" }}>
+      <button onClick={fetchGreeting}>Fetch Greeting</button>
     </div>
   );
 }
